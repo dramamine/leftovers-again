b59c57c4dc65fcb46334625c68d5426d613c562c
Marten <martin@mylifeismetal.com>
2016-01-16 21:32:50 -0800

    Fix KO tests

--- spec/lib/koSpec.js
+++ spec/lib/koSpec.js
@@ -4,14 +4,14 @@ describe('KO', () => {
   describe('predictKO', () => {
     it('should predictably handle False Swipe', () => {
       const kochance = KO.predictKO([40], {
-        maxHP: 200
+        maxhp: 200,
       });
       expect(kochance.turns).toBe(5);
       expect(kochance.chance).toBe(100);
     });
     it('should calculate chance correctly', () => {
       const kochance = KO.predictKO([0, 100], {
-        maxHP: 200
+        maxhp: 200,
       });
       expect(kochance.turns).toBe(2);
       expect(kochance.chance).toBe(25);
