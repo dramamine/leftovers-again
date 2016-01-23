3d17573dbc42bd057faae0a6085e7e35ded75483
Marten <martin@mylifeismetal.com>
2016-01-22 22:39:59 -0800

    WOW looks like normal distribution math works!

--- spec/bots/randombattle/fitness/komoddedSpec.js
+++ spec/bots/randombattle/fitness/komoddedSpec.js
@@ -1,9 +1,9 @@
-import KOModded from '../../../../src/bots/randombattle/fitness/komodded';
+import KO from '../../../../src/bots/randombattle/fitness/komodded';
 
 describe('KOModded', () => {
   describe('_getDistribution', () => {
     it('should get the one I\'m looking for', () => {
-      let distro = KOModded._getDistribution(2, false);
+      let distro = KO._getDistribution(2, false);
       expect(distro).toEqual({
         'mean': 185,
         'variance': 42.5,
@@ -11,7 +11,7 @@ describe('KOModded', () => {
         'hits': 2
       });
 
-      distro = KOModded._getDistribution(2, true);
+      distro = KO._getDistribution(2, true);
       expect(distro).toEqual({
         'mean': 185.53857322314047,
         'variance': 243.75846122665826,
@@ -20,5 +20,35 @@ describe('KOModded', () => {
         'nature': true
       });
     });
+  });
+
+  describe('_simpleGetKOChance', () => {
+    it('should report 0 chance to do too much damage', () => {
+      let res = KO._simpleGetKOChance(50, 2, 125);
+      expect(res).toEqual(0);
+
+      res = KO._simpleGetKOChance(100, 3, 400);
+      expect(res).toEqual(0);
+    });
+    it('should report 100 chance to do little damage', () => {
+      let res = KO._simpleGetKOChance(50, 2, 70);
+      expect(res).toEqual(1);
+
+      res = KO._simpleGetKOChance(100, 3, 200);
+      expect(res).toEqual(1);
+    });
+    it('should find a 50% chance if the mean is 200', () => {
+      spyOn(KO, '_getDistribution').and.returnValue({mean: 200, variance: 200});
+      const res = KO._simpleGetKOChance(100, 2, 200);
+      console.log('komodded chance:', res);
+      expect(res).toBeCloseTo(0.5, 4);
+    });
+    it('should have more than a 50% chance if the damage is higher', () => {
+      spyOn(KO, '_getDistribution').and.returnValue({mean: 200, variance: 200});
+      const res = KO._simpleGetKOChance(105, 2, 200);
+      console.log('komodded chance:', res);
+      expect(res).toBeGreaterThan(0.5);
+    });
+
   });
 });
--- src/bots/randombattle/fitness/komodded.js
+++ src/bots/randombattle/fitness/komodded.js
@@ -1,17 +1,18 @@
 // import typeChart from './typechart';
 // import Damage from './damage';
 import KO from '../../../lib/kochance';
-// import Gaussian from './gaussian';
+import Gaussian from '../../../lib/gaussian';
 
 import distributions from './probability-distributions';
 
 class KOModded extends KO {
-  static _simpleGetKOChance(damage, hp, eot, hits, maxHP, toxicCounter,
-    isNatureKnown = false) {
+  static _simpleGetKOChance(damage, hits, hp, eot = 0, maxHP = 400,
+    toxicCounter = 0, isNatureKnown = false) {
     //
     const dmgTarget = hp - (eot * hits);
     // confirm it's in the range
-    if (dmgTarget <= damage * 0.82 * hits) {
+    // if it's under bad nature * 85%
+    if (dmgTarget <= damage * 0.702479339 * hits) {
       return 1;
     }
     if (dmgTarget >= damage * 1.21 * hits) {
@@ -19,7 +20,7 @@ class KOModded extends KO {
     }
 
     // normalized: how many of this 100% max damage must we do to kill?
-    const dmgPct = 100 * hp / dmgTarget;
+    const dmgPct = 100 * hp / damage;
     console.log('calculations:', damage, hp, dmgTarget, dmgPct);
     const {mean, variance} = this._getDistribution(hits, isNatureKnown);
     return this._normalize(dmgPct, mean, variance);
@@ -36,7 +37,7 @@ class KOModded extends KO {
       && !distro.crits);
   }
 
-  static _normalize(dmgPct) {
+  static _normalize(dmgPct, mean, variance) {
     // const mean = min + (max - min) / 2;
     // let variance = 5.8333;
     // switch (hits) {
@@ -47,8 +48,8 @@ class KOModded extends KO {
     // const distribution = new Gaussian(mean, variance);
     // return distribution.cdf(target);
     //
-    const distribution = new Gaussian(0.5, 0.05);
-    return distribution.cdf(dmgPct);
+    const distribution = new Gaussian(mean, variance);
+    return 1 - distribution.cdf(dmgPct);
   }
 }
 export default KOModded;
