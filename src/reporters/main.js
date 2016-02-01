484f6691034ab80c34f1228718391080f59c031d
Marten <martin@mylifeismetal.com>
2016-01-31 22:40:59 -0800

    Starting to look pretty good with hp bars and specieses

--- src/reporters/main.js
+++ src/reporters/main.js
@@ -11,15 +11,28 @@ const YOUR_TEXT = chalk.bold.black;
 
 class Reporter {
   report(state) {
-    const stuff = `${this.myReserve(state)} ${this.myStatuses(state)} ${this.yourStatuses(state)} ${this.yourReserve(state)}`;
+    const myr = this.myReserve(state.self.reserve);
+    const yor = this.yourReserve(state.opponent.reserve);
+    const mys = this.myStatuses(state.self.active.conditions);
+    const yos = this.myStatuses(state.opponent.active.conditions);
+    const myHp = this.hp(state.self.active.hppct);
+    const yourHp = this.hp(state.opponent.active.hppct);
+    const mySpecies = this.padLeft(state.self.active.species, 10);
+    const yourSpecies = this.padRight(state.opponent.active.species, 10);
+    const stuff = `${myr} ${mys} ${mySpecies} ${myHp} | ${yourHp} ${yourSpecies} ${yos} ${yor}`;
     console.log(stuff);
   }
-
-  myStatuses(state) {
-    return this.padLeft(this.statusString(state.self.active.conditions), 10);
+  hp(hppct) {
+    const blox = Math.floor(hppct / HP_BAR_LENGTH);
+    const antiblox = HP_BAR_LENGTH - blox;
+    return chalk.bgGreen(' '.repeat(blox)) + chalk.bgRed(' '.repeat(antiblox));
   }
-  yourStatuses(state) {
-    return this.padRight(this.statusString(state.opponent.active.conditions), 10);
+
+  myStatuses(conditions) {
+    return this.padLeft(this.statusString(conditions), 10);
+  }
+  yourStatuses(conditions) {
+    return this.padRight(this.statusString(conditions), 10);
   }
   statusString(statuses) {
     if (!statuses || statuses.length === 0) return '          ';
@@ -29,11 +42,11 @@ class Reporter {
     return '[' + statuses.map(str => str.substr(0, 2)).join(' ').substr(0, 8) + ']';
   }
 
-  myReserve(state) {
-    const myAlive = state.self.reserve.filter( mon => {
+  myReserve(reserve) {
+    const myAlive = reserve.filter( mon => {
       return !mon.dead;
     }).length;
-    const myDead = state.self.reserve.filter( mon => {
+    const myDead = reserve.filter( mon => {
       return mon.dead;
     }).length;
 
@@ -42,11 +55,11 @@ class Reporter {
     ));
     return stuff;
   }
-  yourReserve(state) {
-    const yourAlive = state.opponent.reserve.filter( mon => {
+  yourReserve(reserve) {
+    const yourAlive = reserve.filter( mon => {
       return !mon.dead;
     }).length;
-    const yourDead = state.opponent.reserve.filter( mon => {
+    const yourDead = reserve.filter( mon => {
       return mon.dead;
     }).length;
 
@@ -57,9 +70,11 @@ class Reporter {
   }
 
   padLeft(nr, n, str) {
+    if (nr.length >= n) return nr.substr(0, n);
     return Array(n-String(nr).length+1).join(str||' ')+nr;
   }
   padRight(nr, n, str) {
+    if (nr.length >= n) return nr.substr(0, n);
     return nr + Array(n-String(nr).length+1).join(str||' ');
   }
 }
