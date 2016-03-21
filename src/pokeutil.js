8a7ce8815b7d19727b009f349772c039b9a1d29c
Marten <martin@mylifeismetal.com>
2016-03-20 20:55:19 -0700

    Pokeutil docs, tests, & boost combiner function

--- src/pokeutil.js
+++ src/pokeutil.js
@@ -8,22 +8,39 @@ import BattlePokedex from 'data/pokedex';
 import log from 'log';
 
 class PokeUtil {
+  /**
+   * Return the Smogon official ID for a given Pokemon. This works on both
+   * Pokemon 'species' and move 'name' fields. Use this field when you need
+   * the 'standardized' name. These are used when you're looking up stuff from
+   * the data folder.
+   *
+   * @param  {String} text The field to transform.
+   * @return {String}      The ID.
+   */
   toId(text) {
     let name = '';
-    if (text) {
-      // most lines copied from server code..
-      name = ('' + text).replace(/[\|\s\[\]\,\']+/g, '').toLowerCase().trim();
+    if (!text) return name;
 
-      // these two are not! but I needed them.
-      name = name.replace('-', '');
-      name = name.replace('.', '');
-      name = name.replace(' ', '');
+    // most lines copied from server code..
+    name = ('' + text).replace(/[\|\s\[\]\,\']+/g, '').toLowerCase().trim();
 
-      if (name.length > 18) name = name.substr(0, 18).trim();
-    }
+    // these lines are not! but I needed them.
+    name = name.replace('-', '');
+    name = name.replace('.', '');
+    name = name.replace(' ', '');
+
+    if (name.length > 18) name = name.substr(0, 18).trim();
     return name;
   }
 
+  /**
+   * Get an object fulla data about a move.
+   *
+   * Data is sourced both from the official Smogon server and from the Honko
+   * damage calculator. Honko data helps with the damage calculator.
+   * @param  {String} id The move ID.
+   * @return {Move}  A Move object.
+   */
   researchMoveById(id) {
     // hidden power moves end with '60'. hidden power ground comes out as
     // hiddenpowerground6 due to the 18-character limit. it's kept as
@@ -41,12 +58,38 @@ class PokeUtil {
     return Object.assign(battleData, honkoData);
   }
 
+  /**
+   * Get an object fulla data about a Pokemon.
+   *
+   * Data is a limited number of fields from the official Smogon server
+   * data files.
+   *
+   * @param  {String} id The Pokemon ID.
+   * @return {Pokemon}   A Pokemon object.
+   */
   researchPokemonById(id) {
     id = this.toId(id); // eslint-disable-line
     if (BattlePokedex[id]) return BattlePokedex[id];
 
     log.warn('couldn\'t find my pokemon ' + id );
     return {name: id, id};
+  }
+
+  /**
+   * Combine two boost objects. (For example, use this to apply boost effects
+   * to a mon's existing boosts.) Boost objects have keys for their stats
+   * (atk, def, spa, spd, spe) and values representing their boost level.
+   *
+   * @param  {Object} old     A boost object.
+   * @param  {Object} updates A boost object.
+   * @return {Object}         A boost object.
+   */
+  boostCombiner(old = {}, updates = {}) {
+    Object.keys(updates).forEach(boost => {
+      old[boost] = Math.min(6, Math.max(-6,
+        (old[boost] || 0) + updates[boost]));
+    });
+    return old;
   }
 
   /**
