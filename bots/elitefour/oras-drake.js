030f9345d7b16aebce50e4e67486c6fe6e614724
Marten <martin@mylifeismetal.com>
2016-05-08 20:11:30 -0700

    More bots; update to node 6 and its updated transpiler

---
+++ bots/elitefour/oras-drake.js
@@ -0,0 +1,58 @@
+import EliteFour from './elitefour';
+
+export default class Drake extends EliteFour {
+  constructor() {
+    super();
+    this.meta = meta;
+  }
+}
+
+const meta = {
+  format: 'anythinggoes',
+  accepts: 'ALL',
+  nickname: 'EliteFour Drake x',
+  description: 'If you\'re reading this it\'s too late.',
+  team: `
+Altaria
+Ability: Natural Cure
+- Aerial Ace
+- Cotton Guard
+- Dragon Pulse
+- Moonblast
+
+Dragalge
+Ability: Poison Point
+- Dragon Pulse
+- Hydro Pump
+- Sludge Wave
+- Thunderbolt
+
+Kingdra
+Ability: Swift Swim
+- Dragon Pulse
+- Surf
+- Yawn
+- Ice Beam
+
+Flygon
+Ability: Levitate
+- Flamethrower
+- Boomburst
+- Dragon Pulse
+- Screech
+
+Haxorus
+Ability: Rivalry
+- Dragon Claw
+- Earthquake
+- X-Scissor
+- Shadow Claw
+
+Salamence @ Salamencite
+Ability: Intimidate
+- Dragon Rush
+- Zen Headbutt
+- Crunch
+- Thunder Fang
+`
+};
---
+++ bots/elitefour/oras-glacia.js
@@ -0,0 +1,57 @@
+import EliteFour from './elitefour';
+
+export default class Glacia extends EliteFour {
+  constructor() {
+    super();
+    this.meta = meta;
+  }
+}
+
+const meta = {
+  format: 'anythinggoes',
+  accepts: 'ALL',
+  nickname: 'EliteFour Glacia x',
+  team: `
+Abomasnow
+Ability: Snow Warning
+- Blizzard
+- Wood Hammer
+- Ice Shard
+- Earthquake
+
+Beartic
+Ability: Snow Cloak
+- Icicle Crash
+- Slash
+- Shadow Claw
+- Brick Break
+
+Froslass
+Ability: Snow Cloak
+- Draining Kiss
+- Blizzard
+- Hail
+- Shadow Ball
+
+Vanilluxe
+Ability: Ice Body
+- Ice Beam
+- Mirror Coat
+- Freeze-Dry
+- Signal Beam
+
+Walrein
+Ability: Thick Fat
+- Surf
+- Body Slam
+- Blizzard
+- Sheer Cold
+
+Glalie @ Glalitite
+Ability: Inner Focus
+- Protect
+- Ice Shard
+- Hail
+- Freeze-Dry
+`
+};
