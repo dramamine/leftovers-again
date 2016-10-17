2bf4679bc59f0e6bb5c4ce778d87585b71f9249b
marten silbiger <marten@metal-heart.org>
2016-10-16 23:55:23 -0700

    Update bot-builder script

--- bot-builder.sh
+++ bot-builder.sh
@@ -1,33 +1,23 @@
-npm run start:quick -- alice-abreu --scrappy
-npm run start:quick -- anderson-leonardo-oliveira --scrappy
-npm run start:quick -- blank --scrappy
-npm run start:quick -- claudio-junior --scrappy
-npm run start:quick -- demetrius-nunes-alves --scrappy
-npm run start:quick -- elitefour --scrappy
-npm run start:quick -- fabio-damian --scrappy
-npm run start:quick -- felipe-dermann-de-moura --scrappy
-npm run start:quick -- gabriel-cunegato --scrappy
-npm run start:quick -- gabriel-fazenda --scrappy
-npm run start:quick -- gabriel-lanzer-kannenberg --scrappy
-npm run start:quick -- gabriel-nunes-da-silva --scrappy
-npm run start:quick -- guilherme-rosa-nunes --scrappy
-npm run start:quick -- guilherme-schaidhauer-de-castro --scrappy
-npm run start:quick -- gustavo-roman-marangon --scrappy
-npm run start:quick -- infodump --scrappy
-npm run start:quick -- iuri-fontana-de-nardin --scrappy
-npm run start:quick -- joao-pedro-da-costa-nunes --scrappy
-npm run start:quick -- lucas-reis-nunes --scrappy
-npm run start:quick -- luiz-wendt --scrappy
-npm run start:quick -- mateus-ribeiro --scrappy
-npm run start:quick -- matheus-nunes --scrappy
-npm run start:quick -- meetthefakers --scrappy
-npm run start:quick -- moises-abraham-assayag-neto-assayag --scrappy
-npm run start:quick -- randumb --scrappy
-npm run start:quick -- research --scrappy
-npm run start:quick -- riann-lauermann-selegar --scrappy
-npm run start:quick -- splashbot --scrappy
-npm run start:quick -- stabby --scrappy
-npm run start:quick -- stonedbirdie --scrappy
-npm run start:quick -- tiago-duarte --scrappy
-npm run start:quick -- vitor-luis-mauricio-da-silva --scrappy
-npm run start:quick -- whynotbaut --scrappy
+npm run start:quick -- alice-abreu --scrappy --matches=1
+npm run start:quick -- anderson-leonardo-oliveira --scrappy --matches=1
+npm run start:quick -- claudio-junior --scrappy --matches=1
+npm run start:quick -- demetrius-nunes-alves --scrappy --matches=1
+npm run start:quick -- fabio-damian --scrappy --matches=1
+npm run start:quick -- felipe-dermann-de-moura --scrappy --matches=1
+npm run start:quick -- gabriel-cunegato --scrappy --matches=1
+npm run start:quick -- gabriel-fazenda --scrappy --matches=1
+npm run start:quick -- gabriel-lanzer-kannenberg --scrappy --matches=1
+npm run start:quick -- gabriel-nunes-da-silva --scrappy --matches=1
+npm run start:quick -- guilherme-rosa-nunes --scrappy --matches=1
+npm run start:quick -- guilherme-schaidhauer-de-castro --scrappy --matches=1
+npm run start:quick -- gustavo-roman-marangon --scrappy --matches=1
+npm run start:quick -- iuri-fontana-de-nardin --scrappy --matches=1
+npm run start:quick -- joao-pedro-da-costa-nunes --scrappy --matches=1
+npm run start:quick -- lucas-reis-nunes --scrappy --matches=1
+npm run start:quick -- luiz-wendt --scrappy --matches=1
+npm run start:quick -- mateus-ribeiro --scrappy --matches=1
+npm run start:quick -- matheus-nunes --scrappy --matches=1
+npm run start:quick -- moises-abraham-assayag-neto-assayag --scrappy --matches=1
+npm run start:quick -- riann-lauermann-selegar --scrappy --matches=1
+npm run start:quick -- tiago-duarte --scrappy --matches=1
+npm run start:quick -- vitor-luis-mauricio-da-silva --scrappy --matches=1
