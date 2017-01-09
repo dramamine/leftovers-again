7b0c255fbb725213602d538676339ae55e2777d4
marten silbiger <marten@metal-heart.org>
2017-01-08 17:09:55 -0800

    Update format names; handle team with no nature; add SM elite four

---
+++ src/bots/elitefour/sm-kahili.js
@@ -0,0 +1,51 @@
+import EliteFour from './elitefour';
+
+export default class Kahili extends EliteFour {
+  constructor() {
+    super();
+    this.meta = {
+      format: 'gen7pokebankou',
+      accepts: 'ALL',
+      nickname: 'la-sm-kahili',
+      team: `
+Skarmory
+Ability: Keen Eye
+- Steel Wing
+- Night Slash
+- Rock Slide
+- Spikes
+
+Crobat
+Ability: Inner Focus
+IVs: 0 Atk
+- Air Slash
+- Shadow Ball
+- Dark Pulse
+- Sludge Bomb
+
+Oricorio
+Ability: Dancer
+IVs: 0 Atk
+- Revelation Dance
+- Teeter Dance
+- Air Slash
+- Feather Dance
+
+Mandibuzz
+Ability: Big Pecks
+IVs: 0 Atk
+- Snarl
+- Air Slash
+- Roost
+- Toxic
+
+Toucannon @ Flyinium Z
+Ability: Skill Link
+- Bullet Seed
+- Rock Blast
+- Beak Blast
+- Brick Break
+`
+    };
+  }
+}
---
+++ src/bots/elitefour/sm-olivia.js
@@ -0,0 +1,48 @@
+import EliteFour from './elitefour';
+
+export default class Olivia extends EliteFour {
+  constructor() {
+    super();
+    this.meta = {
+      format: 'gen7pokebankru',
+      accepts: 'ALL',
+      nickname: 'la-sm-olivia',
+      team: `
+Relicanth
+Ability: Swift Swim
+- Head Smash
+- Waterfall
+- Yawn
+- Earthquake
+
+Carbink
+Ability: Clear Body
+- Power Gem
+- Moonblast
+- Reflect
+- Stealth Rock
+
+Golem
+Ability: Rock Head
+- Thunder Punch
+- Stone Edge
+- Earthquake
+- Heavy Slam
+
+Probopass
+Ability: Sturdy
+- Earth Power
+- Power Gem
+- Dazzling Gleam
+- Flash Cannon
+
+Lycanroc @ Rockium Z
+Ability: Keen Eye
+- Crunch
+- Stone Edge
+- Brick Break
+- Rock Climb
+`
+    };
+  }
+}
