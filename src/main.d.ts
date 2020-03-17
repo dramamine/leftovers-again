d989f45aee18463ad67742c4e38331845339e63e
Joe Badlato <joebadlato@gmail.com>
2020-03-16 20:06:55 -0400

    Methods instead of properties

--- src/main.d.ts
+++ src/main.d.ts
@@ -9,14 +9,14 @@ export as namespace LeftoversAgain;
 declare namespace LeftoversAgain {
 
 	class AI {
-		decide: (state: State, rqid: number) => Decision;
-		team: (opponent: string) => string;
+		decide(state: State, rqid: number): Decision;
+		team(opponent: string): string;
 	}
 
 	class Battle {
 		constructor(bid: string, connection: Connection, botpath: string);
 
-		abbreviateState: (state: State) => {
+		abbreviateState(state: State): {
 			turn: any;	// TODO
 			self: {
 				active: {
@@ -33,66 +33,66 @@ declare namespace LeftoversAgain {
 				}
 			}
 		};
-		decide: (state: State) => void;
-		forfeit: () => void;
-		formatMessage: (bid: string, choice: Choice, state: BattleState) => string;	// TODO: Choice & BattleState undefined
-		getHelp: () => void;
-		handle: (type: string, message: Array<object>) => void;	// TODO
-		handleCant: (target: string, reason: string, move: string) => void;
-		handleRequest: (json: string) => boolean;
-		handleStart: () => void;
-		handleTeamPreview: () => void;
-		handleTurn: (that: object) => void;	// TODO
-		handleWin: (nick: string) => void;
-		lookupMonIdx: (mons: Array<object>, idx: any) => void; 	// TODO
-		lookupMoveIdx: (moves: Array<object>, idx: any) => void; 	// TODO
-		myBot: () => object;	// TODO
+		decide(state: State): void;
+		forfeit(): void;
+		formatMessage(bid: string, choice: Choice, state: BattleState): string;	// TODO: Choice & BattleState undefined
+		getHelp(): void;
+		handle(type: string, message: Array<object>): void;	// TODO
+		handleCant(target: string, reason: string, move: string): void;
+		handleRequest(json: string): boolean;
+		handleStart(): void;
+		handleTeamPreview(): void;
+		handleTurn(that: object): void;	// TODO
+		handleWin(nick: string): void;
+		lookupMonIdx(mons: Array<object>, idx: any): void; 	// TODO
+		lookupMoveIdx(moves: Array<object>, idx: any): void; 	// TODO
+		myBot(): object;	// TODO
 	}
 
 	class BattleStore {
 		myNick: string;
 		yourNick: string;
 
-		data: () => object;	// TODO
-		handle: (type: string, message: Array<any>) => void;	// TODO
-		handleDetailsChange: (ident: string, species: string, hpstatus: string, reason: string) => void;
-		handleMove: (actor: string, move: string, target: any) => any;	// TODO
-		handlePlayer: (id: string, name: string, something: any) => void;	// TODO
-		handleRequest: (json: string) => void;
-		handleSideEnd: (side: string, action: string) => void;
-		handleSideStart: (side: string, action: string) => void;
-		handleTurn: (x: number) => void;
+		data(): object;	// TODO
+		handle(type: string, message: Array<any>): void;	// TODO
+		handleDetailsChange(ident: string, species: string, hpstatus: string, reason: string): void;
+		handleMove(actor: string, move: string, target: any): any;	// TODO
+		handlePlayer(id: string, name: string, something: any): void;	// TODO
+		handleRequest(json: string): void;
+		handleSideEnd(side: string, action: string): void;
+		handleSideStart(side: string, action: string): void;
+		handleTurn(x: number): void;
 	}
 
 	class Challenger {	// TODO: there are two Challenger classes - with some overlapping methods
 		constructor(scrappy: boolean, format: string);
 
-		acceptable: (challenge: string, accepts: string) => boolean;
-		requiresTeam: (format: any) => any;	// TODO
-		cancelOutstandingChallenges: () => void;
-		challenge: (the: string) => void;
-		challengeNext: () => void;
-		destroy: () => void;
-		gunzBlazing: (args: any) => void;	// TODO
-		onBattleReport: () => any;	// TODO
-		onUpdateChallenges: (msg: string) => void;
-		onUpdateUser: (nick: string, status: number) => void;
-		onUserJoin: (user: string) => void;
-		onUserLeave: (user: string) => void;
+		acceptable(challenge: string, accepts: string): boolean;
+		requiresTeam(format: any): any;	// TODO
+		cancelOutstandingChallenges(): void;
+		challenge(the: string): void;
+		challengeNext(): void;
+		destroy(): void;
+		gunzBlazing(args: any): void;	// TODO
+		onBattleReport(): any;	// TODO
+		onUpdateChallenges(msg: string): void;
+		onUpdateUser(nick: string, status: number): void;
+		onUserJoin(user: string): void;
+		onUserLeave(user: string): void;
 		// below only in models
-		challengeSomeone: (users?: Set<any>) => void;	// TODO
-		sendTeam: (opponent: string) => boolean;
-		tryChallenge: (opponent: any) => any;	// TODO
+		challengeSomeone(users?: Set<any>): void;	// TODO
+		sendTeam(opponent: string): boolean;
+		tryChallenge(opponent: any): any;	// TODO
 	}
 
 	class Connection {
 		constructor();
 
-		close: (message: string) => void;
-		connect: () => void;
-		exit: () => void;
-		handleMessage:(msg: string) => void;
-		send: (message: string) => void;
+		close(message: string): void;
+		connect(): void;
+		exit(): void;
+		handleMessage:(msg: string): void;
+		send(message: string): void;
 	}
 
 	class Decision {
@@ -103,30 +103,30 @@ declare namespace LeftoversAgain {
 	}
 
 	class Interactive {
-		challenge: () => void;
-		onLobbyEnter: () => void;
-		onUpdateUser: (nick: any, status: any) => any;	// TODO
+		challenge(): void;
+		onLobbyEnter(): void;
+		onUpdateUser(nick: any, status: any): any;	// TODO
 	}
 
 	class Listener {
 		constructor();
 
-		relay: (type: string, params: Array<object>) => void; 	// TODO
-		subscribe: (type: string, callback: () => any) => void;	// TODO
-		unsubscribe: (type: string, callback: () => any) => void;	// TODO
+		relay(type: string, params: Array<object>): void; 	// TODO
+		subscribe(type: string, callback: () => any): void;	// TODO
+		unsubscribe(type: string, callback: () => any): void;	// TODO
 	}
 
 	class Lobby {
 		constructor(scrappy: boolean, format: string);
 
-		destroy: () => void;
-		onUpdateUser: (nick: string, status: number) => void;
-		onUserJoin: (user: string) => void;
-		onUserLeave: (user: string) => void;
+		destroy(): void;
+		onUpdateUser(nick: string, status: number): void;
+		onUserJoin(user: string): void;
+		onUserLeave(user: string): void;
 	}
 
 	class Log {
-		setLogLevel: (lvl: number) => void;
+		setLogLevel(lvl: number): void;
 	}
 
 	class Monkey extends Connection {
@@ -135,38 +135,38 @@ declare namespace LeftoversAgain {
 
 	class MOVE extends Decision {
 		constructor(id: number | string | object, target?: number);
-		setMegaEvo: (should: boolean) => void;
-		useZMove: (should: boolean) => void;
+		setMegaEvo(should: boolean): void;
+		useZMove(should: boolean): void;
 	}
 
 	class Pokemon {
 		constructor(ident: string, details: string);
 
-		updateMoveList: (moves: Array<any>) => Array<any>;	// TODO
-		addStatus: (status: string) => void;
-		assimilate: (obj: object) => void;	// TODO
-		data: () => Pokemon;
-		recordMove: (move: string) => void;
-		removeStatus: (status: string) => void;
-		research: (spec: string) => void;
-		useBoost: (stat: string, stage: number) => void;
-		useCondition: (condition: string) => void;
-		useDetails: (details: any, force: boolean) => void;
+		updateMoveList(moves: Array<any>): Array<any>;	// TODO
+		addStatus(status: string): void;
+		assimilate(obj: object): void;	// TODO
+		data(): Pokemon;
+		recordMove(move: string): void;
+		removeStatus(status: string): void;
+		research(spec: string): void;
+		useBoost(stat: string, stage: number): void;
+		useCondition(condition: string): void;
+		useDetails(details: any, force: boolean): void;
 	}
 
 	class Report {
 		constructor();
 
-		data: () => Array<object>; 	// TODO
-		win: (victor: string, store: BattleStore, matchid: string) => Report;
+		data(): Array<object>; 	// TODO
+		win(victor: string, store: BattleStore, matchid: string): Report;
 	}
 
 	class Side {
 		constructor();
 
-		data: () => any;	// TODO
-		digest: (action: string) => void;
-		remove: (action: string) => void;
+		data(): any;	// TODO
+		digest(action: string): void;
+		remove(action: string): void;
 	}
 
 	class SWITCH extends Decision {
@@ -176,24 +176,24 @@ declare namespace LeftoversAgain {
 	class Team {
 		constructor(tm: Array<Pokemon> | string);
 
-		interpretOneSmogon: (the: string) => Pokemon;
-		interpretSmogon: (the: string) => Array<Pokemon>;
-		packTeam: (team: Array<Pokemon>) => string;
-		random: (seed: number) => object;	// TODO
-		seemsValid: (tm: Array<Pokemon>) => boolean;
-		asArray: () => Array<Pokemon>;
-		asUtm: () => string;
+		interpretOneSmogon(the: string): Pokemon;
+		interpretSmogon(the: string): Array<Pokemon>;
+		packTeam(team: Array<Pokemon>): string;
+		random(seed: number): object;	// TODO
+		seemsValid(tm: Array<Pokemon>): boolean;
+		asArray(): Array<Pokemon>;
+		asUtm(): string;
 	}
 
 	class Timer {
 		constructor();
 
-		after: (cb: any, seconds: number) => void;	// TODO
-		ping: () => void;
+		after(cb: any, seconds: number): void;	// TODO
+		ping(): void;
 	}
 
 	class Typechart {
-		compare: (move: string, target: Array<any> | string) => number;	// TODO
+		compare(move: string, target: Array<any> | string): number;	// TODO
 	}
 
 	interface MoveData {
